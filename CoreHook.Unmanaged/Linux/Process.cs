668e83fbfe4c15b1c380e4da72300a70444d1d93
unknown v2 <unknownv2@users.noreply.github.com>
2018-06-16 16:39:38 -0400

    Fix line spacing on linux and macOS injection helpers

--- CoreHook.Unmanaged/Linux/Process.cs
+++ CoreHook.Unmanaged/Linux/Process.cs
@@ -9,27 +9,37 @@ namespace CoreHook.Unmanaged.Linux
 
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern int injectByName(string targetName, string injectionLib, ref int pid);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern int findProcessByName(string targetName);
 
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern int injectByPid(int targetPid, string injectionLib);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern void ptrace_read(int targetPid, long addr, IntPtr vptr, int len);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern void ptrace_write(int targetPid, long addr, byte[] vptr, int len);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern void ptrace_detach(int targetPid);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern void ptrace_cont(int targetPid);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern int ptrace_attach(int targetPid);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern int set_pid(IntPtr handle, int pid);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern int parse_elf(IntPtr handle);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern int print_elf(IntPtr handle);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern IntPtr find_symbol(IntPtr handle,
          [MarshalAs(UnmanagedType.LPStr)] String symbolName,
--- CoreHook.Unmanaged/MacOS/Process.cs
+++ CoreHook.Unmanaged/MacOS/Process.cs
@@ -9,6 +9,7 @@ namespace CoreHook.Unmanaged.MacOS
 
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern int injectByName(string targetName, string injectionLib, ref int pid);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern int findProcessByName(string targetName);
 
@@ -20,6 +21,7 @@ namespace CoreHook.Unmanaged.MacOS
 
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern IntPtr copyMemToProcessByPid(int targetPid, byte[] data, long dataSize);
+
         [DllImport(LIBINJECT, SetLastError = true)]
         public static extern IntPtr injectByPidWithArgs(int targetPid, byte[] parameters, long paramsSize);
     }
