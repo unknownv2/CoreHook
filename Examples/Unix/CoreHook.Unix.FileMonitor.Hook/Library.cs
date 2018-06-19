f85258f0b41efc1a6655671bbee8cf54fb759954
unknown v2 <unknownv2@users.noreply.github.com>
2018-06-19 18:01:29 -0400

    Remove unused library load utilities class

--- Examples/Unix/CoreHook.Unix.FileMonitor.Hook/Library.cs
+++ Examples/Unix/CoreHook.Unix.FileMonitor.Hook/Library.cs
@@ -78,7 +78,7 @@ namespace CoreHook.Unix.FileMonitor.Hook
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 dllLoadUtils = new ImportUtils.LibLoaderMacOS();
-                dllHandle = dllLoadUtils.LoadLibrary("/usr/lib/libSystem.dylib");
+                dllHandle = dllLoadUtils.LoadLibrary("/usr/lib/libc.dylib");
             }
 
             var functionHandle = dllLoadUtils.GetProcAddress(dllHandle, "open");
@@ -157,50 +157,5 @@ namespace CoreHook.Unix.FileMonitor.Hook
                 }
             }
         }
-    }
-    interface DllLoadUtils
-    {
-        IntPtr LoadLibrary(string fileName);
-        void FreeLibrary(IntPtr handle);
-        IntPtr GetProcAddress(IntPtr dllHandle, string name);
-    }
-    internal class DllLoadUtilsLinux : DllLoadUtils
-    {
-        public IntPtr LoadLibrary(string fileName)
-        {
-            return dlopen(fileName, RTLD_NOW);
-        }
-
-        public void FreeLibrary(IntPtr handle)
-        {
-            dlclose(handle);
-        }
-
-        public IntPtr GetProcAddress(IntPtr dllHandle, string name)
-        {
-            // clear previous errors if any
-            dlerror();
-            var res = dlsym(dllHandle, name);
-            var errPtr = dlerror();
-            if (errPtr != IntPtr.Zero)
-            {
-                throw new Exception("dlsym: " + Marshal.PtrToStringAnsi(errPtr));
-            }
-            return res;
-        }
-
-        const int RTLD_NOW = 2;
-
-        [DllImport("libdl.so")]
-        private static extern IntPtr dlopen(String fileName, int flags);
-
-        [DllImport("libdl.so")]
-        private static extern IntPtr dlsym(IntPtr handle, String symbol);
-
-        [DllImport("libdl.so")]
-        private static extern int dlclose(IntPtr handle);
-
-        [DllImport("libdl.so")]
-        private static extern IntPtr dlerror();
     }
 }
