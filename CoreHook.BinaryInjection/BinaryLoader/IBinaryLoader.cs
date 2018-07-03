8b8074003cfec85028a4e991cfade18281f53df1
unknown v2 <unknownv2@users.noreply.github.com>
2018-07-02 23:27:07 -0400

    Add CallFunctionWithRemoteArgs with RemoteFunctionArgs to IBinaryLoader

--- CoreHook.BinaryInjection/BinaryLoader/IBinaryLoader.cs
+++ CoreHook.BinaryInjection/BinaryLoader/IBinaryLoader.cs
@@ -8,6 +8,7 @@ namespace CoreHook.BinaryInjection
     {
         void Load(Process targetProcess, string binaryPath, IEnumerable<string> dependencies = null, string dir = null);
         void ExecuteWithArgs(Process process, string module, object args);
+        void CallFunctionWithRemoteArgs(Process process, string module, string function, RemoteFunctionArgs arguments);
         void CallFunctionWithRemoteArgs(Process process, string module, string function, IntPtr arguments);
         IntPtr CopyMemoryTo(Process proc, byte[] buffer, uint length);
     }
