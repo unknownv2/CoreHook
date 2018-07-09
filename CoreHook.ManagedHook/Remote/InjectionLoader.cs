fef8a86d7c439a024efeb726e4dd7c7db8eeb0c1
unknown v2 <unknownv2@users.noreply.github.com>
2018-07-08 20:49:05 -0400

    Implement InjectionLoader
    - Server handling request from client and processing completion

--- CoreHook.ManagedHook/Remote/InjectionLoader.cs
+++ CoreHook.ManagedHook/Remote/InjectionLoader.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO.Pipes;
 using System.Threading;
+using CoreHook.IPC.NamedPipes;
+using CoreHook.IPC.Platform;
 
 namespace CoreHook.ManagedHook.Remote
 {
@@ -13,6 +15,45 @@ namespace CoreHook.ManagedHook.Remote
             public Mutex ThreadLock = new Mutex(false);
             public ManualResetEvent Completion = new ManualResetEvent(false);
             public Exception Error = null;
+        }
+
+        public static NamedPipeServer CreateServer(string namedPipeName, IPipePlatform pipePlatform)
+        {
+            return NamedPipeServer.StartNewServer(namedPipeName, pipePlatform, HandleRequest);
+        }
+
+        private static void HandleRequest(string request, NamedPipeServer.Connection connection)
+        {
+            NamedPipeMessages.Message message = NamedPipeMessages.Message.FromString(request);
+
+            switch (message.Header)
+            {
+                case NamedPipeMessages.InjectionCompleteNotification.InjectionComplete:
+                    var msg = new NamedPipeMessages.InjectionCompleteNotification(message.Body);
+                    var reqData = msg.RequestData;
+                    if (reqData.Completed)
+                    {
+                        InjectionCompleted(reqData.PID);
+                    }
+                    break;
+            }
+        }
+
+        public static bool SendInjectionComplete(string pipeName, int pid)
+        {
+            using (NamedPipeClient pipeClient = new NamedPipeClient(pipeName))
+            {
+                if (pipeClient.Connect())
+                {
+                    var request = new NamedPipeMessages.InjectionCompleteNotification(pid, true);
+                    if (pipeClient.TrySendRequest(request.CreateMessage()))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
         private static SortedList<Int32, InjectionWait> InjectionList = new SortedList<Int32, InjectionWait>();
         public static void BeginInjection(int InTargetPID)
@@ -39,10 +80,58 @@ namespace CoreHook.ManagedHook.Remote
                     InjectionList.Add(InTargetPID, WaitInfo);
             }
         }
-    }
-    public interface IInjectionHelper
-    {
+        public static void EndInjection(int InTargetPID)
+        {
+            lock (InjectionList)
+            {
+                InjectionList[InTargetPID].ThreadLock.ReleaseMutex();
 
+                InjectionList.Remove(InTargetPID);
+            }
+        }
+        public static void WaitForInjection(int InTargetPID)
+        {
+            InjectionWait WaitInfo;
+
+            lock (InjectionList)
+            {
+                WaitInfo = InjectionList[InTargetPID];
+            }
+
+            if (!WaitInfo.Completion.WaitOne(20000, false))
+                throw new TimeoutException("Unable to wait for injection completion.");
+
+            if (WaitInfo.Error != null)
+                throw WaitInfo.Error;
+        }
+
+        public static void InjectionException(
+            int clientPID,
+            Exception e)
+        {
+            InjectionWait WaitInfo;
+
+            lock (InjectionList)
+            {
+                WaitInfo = InjectionList[clientPID];
+            }
+
+            WaitInfo.Error = e;
+            WaitInfo.Completion.Set();
+        }
+
+        public static void InjectionCompleted(int clientPID)
+        {
+            InjectionWait WaitInfo;
+
+            lock (InjectionList)
+            {
+                WaitInfo = InjectionList[clientPID];
+            }
+
+            WaitInfo.Error = null;
+            WaitInfo.Completion.Set();
+        }
     }
 
     public class InjectionPrepareEventArgs : EventArgs
