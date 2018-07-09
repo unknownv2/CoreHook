f108b43b1e37397c246ee422896e04990a7d90c6
unknown v2 <unknownv2@users.noreply.github.com>
2018-07-08 22:22:28 -0400

    Add boolean return FreeMemory function to IBinaryLoader

--- CoreHook.BinaryInjection/BinaryLoader/IBinaryLoader.cs
+++ CoreHook.BinaryInjection/BinaryLoader/IBinaryLoader.cs
@@ -11,5 +11,6 @@ namespace CoreHook.BinaryInjection
         void CallFunctionWithRemoteArgs(Process process, string module, string function, RemoteFunctionArgs arguments);
         void CallFunctionWithRemoteArgs(Process process, string module, string function, IntPtr arguments);
         IntPtr CopyMemoryTo(Process proc, byte[] buffer, uint length);
+        bool FreeMemory(Process proc, IntPtr address, uint length);
     }
 }
