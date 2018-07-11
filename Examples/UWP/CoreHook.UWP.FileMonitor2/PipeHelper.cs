3d646238eeff266caed5ba1a6c95c007c6c18ede
unknown v2 <unknownv2@users.noreply.github.com>
2018-07-11 17:54:39 -0400

    Create PipeHelper hoooking class
    - Helper class to hook CreateNamedPipeW to enable desired SID on the Pipe

---
+++ Examples/UWP/CoreHook.UWP.FileMonitor2/PipeHelper.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreHook.FileMonitor.Service;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.IO.Pipes;
+using System.IO;
+
+namespace CoreHook.UWP.FileMonitor2
+{
+    public class PipeHelper
+    {
+        internal static partial class Libraries
+        {
+            internal const string Kernel32 = "kernel32.dll";
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct SECURITY_ATTRIBUTES
+        {
+            internal uint nLength;
+            internal IntPtr lpSecurityDescriptor;
+            internal bool bInheritHandle;
+        }
+
+        [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true,
+            BestFitMapping = false, EntryPoint = "CreateNamedPipeW",
+            CallingConvention = CallingConvention.StdCall)]
+        internal static extern IntPtr CreateNamedPipe(
+            string pipeName,
+            int openMode,
+            int pipeMode,
+            int maxInstances,
+            int outBufferSize,
+            int inBufferSize,
+            int defaultTimeout,
+            ref SECURITY_ATTRIBUTES securityAttributes);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall,
+             CharSet = CharSet.Unicode,
+             SetLastError = true)]
+        delegate IntPtr DCreateNamedPipe(
+            string pipeName,
+            int openMode,
+            int pipeMode,
+            int maxInstances,
+            int outBufferSize,
+            int inBufferSize,
+            int defaultTimeout,
+            ref SECURITY_ATTRIBUTES securityAttributes);
+
+        private string PipeName;
+
+        public PipeHelper(string pipeName)
+        {
+            PipeName = pipeName;
+        }
+        public void Start()
+        {
+            var clientTask = RunClientAsync();
+
+            // Wait for the client to exit.
+            clientTask.GetAwaiter().GetResult();
+        }
+        private async Task RunClientAsync()
+        {
+            await Task.Yield();
+
+            CreateHooks();
+
+            try
+            {
+                while (true)
+                {
+                    Thread.Sleep(500);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex.ToString());
+            }
+        }
+
+        private static void Log(object msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private static PipeSecurity CreateUWPPipeSecurity()
+        {
+            const PipeAccessRights access = PipeAccessRights.ReadWrite;
+
+            var sec = new PipeSecurity();
+
+            using (var identity = WindowsIdentity.GetCurrent())
+            {
+                sec.AddAccessRule(
+                    new PipeAccessRule(identity.User, access, AccessControlType.Allow)
+                );
+
+                if (identity.User != identity.Owner)
+                {
+                    sec.AddAccessRule(
+                        new PipeAccessRule(identity.Owner, access, AccessControlType.Allow)
+                    );
+                }
+            }
+            // Allow all app packages to connect.     
+            sec.AddAccessRule(new PipeAccessRule(new SecurityIdentifier("S-1-15-2-1"), access, AccessControlType.Allow));
+            return sec;
+        }
+        internal static unsafe SECURITY_ATTRIBUTES GetSecAttrs(HandleInheritability inheritability, PipeSecurity pipeSecurity, ref GCHandle pinningHandle)
+        {
+            SECURITY_ATTRIBUTES secAttrs = default(SECURITY_ATTRIBUTES);
+            secAttrs.nLength = (uint)sizeof(SECURITY_ATTRIBUTES);
+
+            if ((inheritability & HandleInheritability.Inheritable) != 0)
+            {
+                secAttrs.bInheritHandle = true;
+            }
+
+            if (pipeSecurity != null)
+            {
+                byte[] securityDescriptor = pipeSecurity.GetSecurityDescriptorBinaryForm();
+                pinningHandle = GCHandle.Alloc(securityDescriptor, GCHandleType.Pinned);
+                fixed (byte* pSecurityDescriptor = securityDescriptor)
+                {
+                    secAttrs.lpSecurityDescriptor = (IntPtr)pSecurityDescriptor;
+                }
+            }
+
+            return secAttrs;
+        }
+        Stack<String> Queue = new Stack<String>();
+        LocalHook CreateNamedPipeHook;
+        public bool Started = false;
+        // this is where we are intercepting all file accesses!
+        private static IntPtr CreateNamedPipe_Hooked(
+            string pipeName,
+            int openMode,
+            int pipeMode,
+            int maxInstances,
+            int outBufferSize,
+            int inBufferSize,
+            int defaultTimeout,
+            ref SECURITY_ATTRIBUTES securityAttributes)
+
+        {
+            Log($"Creating pipe: '{pipeName}'...");
+
+            try
+            {
+                PipeHelper This = (PipeHelper)HookRuntimeInfo.Callback;
+                if (This != null)
+                {
+
+                    if (pipeName.Contains(This.PipeName))
+                    {
+                        var pinningHandle = new GCHandle();
+                        IntPtr result = IntPtr.Zero;
+                        try
+                        {
+                            var security = GetSecAttrs(
+                                HandleInheritability.None,
+                                CreateUWPPipeSecurity(),
+                                ref pinningHandle);
+
+                            Log("Returning modified pipe");
+
+                            result = CreateNamedPipe(
+                               pipeName,
+                                openMode,
+                                pipeMode,
+                                maxInstances,
+                                outBufferSize,
+                                inBufferSize,
+                                defaultTimeout,
+                                ref security);
+                        }
+                        finally
+                        {
+                            if (pinningHandle.IsAllocated)
+                            {
+                                pinningHandle.Free();
+                            }
+                        }
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
+            }
+            // call original API...
+            return CreateNamedPipe(
+               pipeName,
+                openMode,
+                pipeMode,
+                maxInstances,
+                outBufferSize,
+                inBufferSize,
+                defaultTimeout,
+                ref securityAttributes);
+        }
+        private void CreateHooks()
+        {
+            Log("Adding hook to kernel32.dll!CreateNamedPipeW");
+
+            CreateNamedPipeHook = LocalHook.Create(
+                LocalHook.GetProcAddress(Libraries.Kernel32, "CreateNamedPipeW"),
+                new DCreateNamedPipe(CreateNamedPipe_Hooked),
+                this);
+
+            CreateNamedPipeHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
+
+            Started = true;
+        }
+    }
+}
--- Examples/UWP/CoreHook.UWP.FileMonitor2/Program.cs
+++ Examples/UWP/CoreHook.UWP.FileMonitor2/Program.cs
@@ -18,243 +18,9 @@ using CoreHook.FileMonitor.Service.Pipe;
 
 namespace CoreHook.UWP.FileMonitor2
 {
-    public class Library
-    {
-        internal static partial class Libraries
-        {
-            internal const string Kernel32 = "kernel32.dll";
-        }
-        private string PipeName;
 
-        public Library(string pipeName)
-        {
-            PipeName = pipeName;
-        }
-        public void Start()
-        {
-            var clientTask = RunClientAsync();
-
-            // Wait for the client to exit.
-            clientTask.GetAwaiter().GetResult();
-        }
-        private async Task RunClientAsync()
-        {
-            await Task.Yield(); // We want this task to run on another thread.
-
-            CreateHooks();
-
-            try
-            {
-                while (true)
-                {
-                    Thread.Sleep(500);
-                }
-            }
-            catch (Exception ex)
-            {
-                ClientWriteLine(ex.ToString());
-            }
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct SECURITY_ATTRIBUTES
-        {
-            internal uint nLength;
-            internal IntPtr lpSecurityDescriptor;
-            internal bool bInheritHandle;
-        }
-
-        [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, SetLastError = true,
-            BestFitMapping = false, EntryPoint = "CreateNamedPipeW",
-            CallingConvention = CallingConvention.StdCall)]
-        internal static extern IntPtr CreateNamedPipe(
-            string pipeName,
-            int openMode,
-            int pipeMode,
-            int maxInstances,
-            int outBufferSize,
-            int inBufferSize,
-            int defaultTimeout,
-            ref SECURITY_ATTRIBUTES securityAttributes);
-
-        [UnmanagedFunctionPointer(CallingConvention.StdCall,
-             CharSet = CharSet.Unicode,
-             SetLastError = true)]
-        delegate IntPtr DCreateNamedPipe(
-            string pipeName,
-            int openMode,
-            int pipeMode,
-            int maxInstances,
-            int outBufferSize,
-            int inBufferSize,
-            int defaultTimeout,
-            ref SECURITY_ATTRIBUTES securityAttributes);
-
-
-        private static void ClientWriteLine(object msg)
-        {
-            Console.WriteLine(msg);
-        }
-
-        private static PipeSecurity CreateUWPPipeSecurity()
-        {
-            const PipeAccessRights access = PipeAccessRights.ReadWrite;
-
-            var sec = new PipeSecurity();
-
-            using (var identity = WindowsIdentity.GetCurrent())
-            {
-                sec.AddAccessRule(
-                    new PipeAccessRule(identity.User, access, AccessControlType.Allow)
-                );
-
-                if (identity.User != identity.Owner)
-                {
-                    sec.AddAccessRule(
-                        new PipeAccessRule(identity.Owner, access, AccessControlType.Allow)
-                    );
-                }
-            }
-            // And the user's Admin user.
-            sec.AddAccessRule(
-               new PipeAccessRule(
-                   new SecurityIdentifier(WellKnownSidType.AuthenticatedUserSid, null), access, AccessControlType.Allow)
-            );
-            System.Security.Principal.SecurityIdentifier sid = new System.Security.Principal.SecurityIdentifier(System.Security.Principal.WellKnownSidType.WorldSid, null);
-            PipeAccessRule psRule = new PipeAccessRule(sid, access, System.Security.AccessControl.AccessControlType.Allow);
-            // Allow all app packages to connect.
-            sec.AddAccessRule(psRule);
-            sec.AddAccessRule(new PipeAccessRule(new SecurityIdentifier("S-1-15-2-1"), access, AccessControlType.Allow));
-            return sec;
-        }
-        internal static unsafe SECURITY_ATTRIBUTES GetSecAttrs(HandleInheritability inheritability, PipeSecurity pipeSecurity, ref GCHandle pinningHandle)
-        {
-            SECURITY_ATTRIBUTES secAttrs = default(SECURITY_ATTRIBUTES);
-            secAttrs.nLength = (uint)sizeof(SECURITY_ATTRIBUTES);
-
-            if ((inheritability & HandleInheritability.Inheritable) != 0)
-            {
-                secAttrs.bInheritHandle = true;
-            }
-
-            if (pipeSecurity != null)
-            {
-                byte[] securityDescriptor = pipeSecurity.GetSecurityDescriptorBinaryForm();
-                pinningHandle = GCHandle.Alloc(securityDescriptor, GCHandleType.Pinned);
-                fixed (byte* pSecurityDescriptor = securityDescriptor)
-                {
-                    secAttrs.lpSecurityDescriptor = (IntPtr)pSecurityDescriptor;
-                }
-            }
-
-            return secAttrs;
-        }
-        Stack<String> Queue = new Stack<String>();
-        LocalHook CreateNamedPipeHook;
-        public bool Started = false;
-        // this is where we are intercepting all file accesses!
-        private static IntPtr CreateNamedPipe_Hooked(
-            string pipeName,
-            int openMode,
-            int pipeMode,
-            int maxInstances,
-            int outBufferSize,
-            int inBufferSize,
-            int defaultTimeout,
-            ref SECURITY_ATTRIBUTES securityAttributes)
-
-        {
-            ClientWriteLine($"Creating pipe: '{pipeName}'...");
-
-            try
-            {
-                Library This = (Library)HookRuntimeInfo.Callback;
-                if (This != null)
-                {
-
-                    if (pipeName.Contains(This.PipeName))
-                    {
-                        var pinningHandle = new GCHandle();
-                        IntPtr result = IntPtr.Zero;
-                        try
-                        {
-                            var security = GetSecAttrs(
-                                HandleInheritability.None,
-                                CreateUWPPipeSecurity(),
-                                ref pinningHandle);
-
-                            ClientWriteLine("Returning modified pipe");
-
-                            result = CreateNamedPipe(
-                               pipeName,
-                                openMode,
-                                pipeMode,
-                                maxInstances,
-                                outBufferSize,
-                                inBufferSize,
-                                defaultTimeout,
-                                ref security);
-                        }
-                        finally
-                        {
-                            if (pinningHandle.IsAllocated)
-                            {
-                                pinningHandle.Free();
-                            }
-                        }
-                        return result;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ClientWriteLine(ex);
-            }
-            // call original API...
-            return CreateNamedPipe(
-               pipeName,
-                openMode,
-                pipeMode,
-                maxInstances,
-                outBufferSize,
-                inBufferSize,
-                defaultTimeout,
-                ref securityAttributes);
-        }
-        private void CreateHooks()
-        {
-            ClientWriteLine("Adding hook to kernel32.dll!CreateNamedPipeW");
-
-            CreateNamedPipeHook = LocalHook.Create(
-                LocalHook.GetProcAddress(Libraries.Kernel32, "CreateNamedPipeW"),
-                new DCreateNamedPipe(CreateNamedPipe_Hooked),
-                this);
-
-            CreateNamedPipeHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
-
-            Started = true;
-        }
-    }
     class Program
     {
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct SECURITY_ATTRIBUTES
-        {
-            internal uint nLength;
-            internal IntPtr lpSecurityDescriptor;
-            internal bool bInheritHandle;
-        }
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true, EntryPoint = "CreateNamedPipeW",
-    CallingConvention = CallingConvention.StdCall)]
-        internal static extern IntPtr CreateNamedPipe(
-    [MarshalAs(UnmanagedType.LPWStr)]string pipeName,
-    int openMode,
-    int pipeMode,
-    int maxInstances,
-    int outBufferSize,
-    int inBufferSize,
-    int defaultTimeout,
-    SECURITY_ATTRIBUTES securityAttributes);
         private static readonly IJsonRpcContractResolver myContractResolver = new JsonRpcContractResolver
         {
             // Use camelcase for RPC method names.
@@ -264,30 +30,27 @@ namespace CoreHook.UWP.FileMonitor2
         };
 
         private const string CoreHookPipeName = "CoreHook";
-        static Library library = new Library(CoreHookPipeName);
-        static Thread thread;
+        static PipeHelper pipeHelper = new PipeHelper(CoreHookPipeName);
         private static bool IsArchitectureArm()
         {
             var arch = RuntimeInformation.ProcessArchitecture;
             return arch == Architecture.Arm || arch == Architecture.Arm64;
         }
-        private static void CreateLib()
+        private static void CreatePipeHelper()
         {
-            library.Start();
+            pipeHelper.Start();
         }
-        static void Main(string[] args)
+        private static void WaitForHook()
         {
-            thread = new Thread(CreateLib);
-            thread.Start();
-            while(!library.Started)
+            while (!pipeHelper.Started)
             {
                 Thread.Sleep(500);
             }
-            /*
-            SECURITY_ATTRIBUTES sec = new SECURITY_ATTRIBUTES();
-            var pipeTest = CreateNamedPipe(@"\\.\pipe\CoreHookInjection",
-                0, 0, 0, 0, 0, 0, sec);
-            Console.WriteLine($"CreateNamedPipe returned {pipeTest.ToInt32()}");*/
+        }
+        static void Main(string[] args)
+        {   
+            Task.Factory.StartNew(() => CreatePipeHelper(), TaskCreationOptions.LongRunning);
+            WaitForHook();
 
             int TargetPID = 0;
 
@@ -333,8 +96,8 @@ namespace CoreHook.UWP.FileMonitor2
             string coreHookDll = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
                 Environment.Is64BitProcess ? "corehook64.dll" : "corehook32.dll");
 
-            //GrantAllAppPkgsAccessToDir(currentDir);
-            //GrantAllAppPkgsAccessToDir(Path.Combine(currentDir, "../netstandard2.0"));
+            GrantAllAppPkgsAccessToDir(currentDir);
+            GrantAllAppPkgsAccessToDir(Path.Combine(currentDir, "../netstandard2.0"));
 
             // start process and begin dll loading
             if (!string.IsNullOrEmpty(targetApp))
@@ -389,11 +152,6 @@ namespace CoreHook.UWP.FileMonitor2
                 return;
             }
 
-            /*
-            ManagedHook.Remote.RemoteHooking.Inject(
-                procId,
-                coreHookDll);
-            */
             ManagedHook.Remote.RemoteHooking.Inject(
                 procId,
                 coreRunDll,
