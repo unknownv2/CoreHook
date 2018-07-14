565a0e4e3ac563673e5703764dc720f99cff0ca9
unknown v2 <unknownv2@users.noreply.github.com>
2018-07-14 16:46:50 -0400

    Convert FileMonitor example to .NET Core 2.1

--- Examples/CoreHook.FileMonitor/Program.cs
+++ Examples/CoreHook.FileMonitor/Program.cs
@@ -192,7 +192,7 @@ namespace CoreHook.FileMonitor
                 injectionLibrary,
                 injectionLibrary,
                 new PipePlatform(),
-                new string[] { coreHookDll },
+                new []{ coreHookDll },
                 CoreHookPipeName);
         }
 
