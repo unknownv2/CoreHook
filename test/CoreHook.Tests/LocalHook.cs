4ce6c0bc7100cb60d60adc55e19f8d2f2e13557a
unknown v2 <unknownv2@users.noreply.github.com>
2018-08-31 00:21:03 -0400

    Add initial test project using xunit

---
+++ test/CoreHook.Tests/LocalHook.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CoreHook.Tests
+{
+    public class TestLocalHook
+    {
+        [Fact]
+        public void SimpleHook()
+        {
+
+        }
+    }
+}
