2f8e0dd675a6b7235cb02e052a0c6437b7129cd8
unknown v2 <unknownv2@users.noreply.github.com>
2018-09-08 18:01:36 -0400

    Add exec function
    * Using build scripts frm github.com/natemcmaster/DotNetCorePlugins

--- build.ps1
+++ build.ps1
@@ -28,5 +28,16 @@ if ($ci) {
     $MSBuildArgs += '-p:CI=true'
 }
 
+function exec([string]$_cmd) {
+    write-host -ForegroundColor DarkGray ">>> $_cmd $args"
+    $ErrorActionPreference = 'Continue'
+    & $_cmd @args
+    $ErrorActionPreference = 'Stop'
+    if ($LASTEXITCODE -ne 0) {
+        write-error "Failed with exit code $LASTEXITCODE"
+        exit 1
+    }
+}
+
 exec dotnet build --configuration $Configuration '-warnaserror:CS1591' @MSBuildArgs
 
