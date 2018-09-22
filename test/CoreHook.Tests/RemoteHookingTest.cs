06cb1927c283fddadf4ec72bba3a47cdfad2e7f3
Thierry <unknownv2@users.noreply.github.com>
2018-09-21 20:53:01 -0400

    Add remotehooking test

--- test/CoreHook.Tests/RemoteHookingTest.cs
+++ test/CoreHook.Tests/RemoteHookingTest.cs
@@ -1,11 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 using Xunit;
+using CoreHook.ManagedHook.Remote;
+using CoreHook.ManagedHook.ProcessUtils;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 
 namespace CoreHook.Tests
 {
     public class RemoteHookingTest
     {
+        [Fact]
+        private void TestRemoteInject()
+        {
+            const string TestHookLibrary = "CoreHook.Tests.SimpleHook1.dll";
+            const string TestMessage = "Berner";
+
+            InjectDllIntoTarget(Resources.TargetProcess,
+               Resources.GetTestDllPath(
+               TestHookLibrary
+               ),
+               TestMessage);
+
+            Assert.Equal(TestMessage, Resources.ReadFromProcess(Resources.TargetProcess));
+
+            Resources.EndTestProcess();
+        }
+
+        private void InjectDllIntoTarget(Process target, string injectionLibrary, string message)
+        {
+            string coreRunDll, coreLibrariesPath, coreRootPath, coreLoadDll, coreHookDll;
+            if (Resources.GetCoreLoadPaths(
+                out coreRunDll, out coreLibrariesPath,
+                out coreRootPath, out coreLoadDll, out coreHookDll))
+            {
+                RemoteHooking.Inject(
+                    target.Id,
+                    coreRunDll,
+                    coreLoadDll,
+                    coreRootPath,
+                    coreLibrariesPath, 
+                    injectionLibrary,
+                    injectionLibrary,
+                    new PipePlatformBase(),
+                    new[] { coreHookDll },
+                    message);
+            }
+        }
     }
 }
