393626ac3537319961c3be2a2aceaf15930c84ff
Thierry <unknownv2@users.noreply.github.com>
2018-09-23 19:22:47 -0400

    Add named pipe tests

--- test/CoreHook.Tests/NamedPipeTest.cs
+++ test/CoreHook.Tests/NamedPipeTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CoreHook.IPC.NamedPipes;
+using CoreHook.IPC.Platform;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,5 +9,48 @@ namespace CoreHook.Tests
 {
     public class NamedPipeTest
     {
+        private const string NamedPipeName = "NamedPipeNameTest";
+
+        [Fact]
+        private void ShouldConnectToServer()
+        {
+            bool receivedMessage = false;
+            const string testMessage = "TestMessage";
+
+            using (var pipeServer = CreateServer(NamedPipeName, new PipePlatformBase(),
+                (string request, NamedPipeServer.Connection connection) =>
+                {
+                    if (request == testMessage)
+                    {
+                        receivedMessage = true;
+                    }
+                    connection.TrySendResponse(request);
+                }))
+            {
+                using (INamedPipeClient pipeClient = new NamedPipeClient(NamedPipeName))
+                {
+                    if(SendPipeMessage(pipeClient, testMessage))
+                    {
+                        Assert.Equal(pipeClient.ReadRawResponse(), testMessage);
+                    }
+                }
+            }
+            Assert.True(receivedMessage);
+        }
+
+        private static INamedPipeServer CreateServer(string namedPipeName, IPipePlatform pipePlatform, Action<string, NamedPipeServer.Connection> handleRequest)
+        {
+            return NamedPipeServer.StartNewServer(namedPipeName, pipePlatform, handleRequest);
+        }
+
+        private static bool SendPipeMessage(INamedPipeClient pipeClient, string message)
+        {
+            if (pipeClient.Connect(3000))
+            {
+                pipeClient.SendRequest(message);
+                return true;
+            }
+            return false;
+        }
     }
 }
