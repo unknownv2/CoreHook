e7549e8cbdc44f4da147b0de966636d626d3fe39
Thierry <unknownv2@users.noreply.github.com>
2018-09-22 20:41:29 -0400

    Add details and comments  to RemoteHookingConfig interface properties

--- src/CoreHook.ManagedHook/Remote/RemoteHookingConfig.cs
+++ src/CoreHook.ManagedHook/Remote/RemoteHookingConfig.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace CoreHook.ManagedHook.Remote
 {
-    public class ProcessCreationConfig: ICreateProcessConfig
+    public class ProcessCreationConfig : ICreateProcessConfig
     {
-        public string ExePath { get; set; }
+        public string ExecutablePath { get; set; }
         public string CommandLine { get; set; }
         public uint ProcessCreationFlags { get; set; }
     }
@@ -25,26 +25,70 @@ namespace CoreHook.ManagedHook.Remote
     }
     public interface ICreateProcessConfig
     {
-        string ExePath { get; set;  }
+        /// <summary>
+        /// Filepath to an executable program on the system that is to be launched.
+        /// </summary>
+        string ExecutablePath { get; set;  }
+        /// <summary>
+        /// Arguments to be passed to the executable program when it is started
+        /// </summary>
         string CommandLine { get; set; }
+        /// <summary>
+        /// Attributes and options passed to the process creation function call.
+        /// </summary>
         uint ProcessCreationFlags { get; set; }
     }
     public interface ICoreHookConfig
     {
+        /// <summary>
+        /// Library that implements function intercept exports for the LocalHook class
+        /// such as DetourInstallHook.
+        /// </summary>
         string DetourLibrary { get; set; }
+        /// <summary>
+        /// .NET library that is loaded and executed inside the target process
+        /// by the boostrap library after starting the .NET Core runtime.
+        /// </summary>
         string PayloadLibrary { get; set; }
     }
     public interface ICoreLoadConfig
     {
+        /// <summary>
+        /// Library that resolves dependencies and passes arguments to
+        /// the .NET payload Assembly.
+        /// </summary>
         string CLRBootstrapLibrary { get; set; }
+        /// <summary>
+        /// Directory path which contains the main CoreCLR modules for hosting the runtime
+        /// such as CoreCLR and clrjit libraries.
+        /// </summary>
         string CoreCLRPath { get; set; }
+        /// <summary>
+        /// Directory path which contains the CoreCLR Assembly reference
+        /// runtime libraries such as System.Private.CoreLib.dll.
+        /// </summary>
         string CoreCLRLibrariesPath { get; set; }
     }
     public interface ICoreRunConfig
     {        
+        /// <summary>
+        /// Library that initializes the .NET Core runtime (CoreCLR) and allows
+        /// loading and executing .NET Assemblies.
+        /// </summary>
         string HostLibrary{ get; set; }
+        /// <summary>
+        /// Option to enable the logging module inside the HostLibrary.
+        /// </summary>
         bool VerboseLog { get; set; }
+        /// <summary>
+        /// Option to enable waiting for a debugger to attach before any 
+        /// .NET Assemblies are loaded by the HostLibrary.
+        /// </summary>
         bool WaitForDebugger { get; set; }
+        /// <summary>
+        /// Option to immediately execute an .NET Assembly after initializing
+        /// the .NET Core runtime.
+        /// </summary>
         bool StartAssembly { get; set; }
     }
 }
