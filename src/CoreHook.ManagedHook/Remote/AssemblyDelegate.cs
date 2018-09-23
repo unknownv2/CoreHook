a98da842fdb99b1764b7cba6ed47231e02b8e6d5
Thierry <unknownv2@users.noreply.github.com>
2018-09-22 22:22:04 -0400

    Add AssemblyDelegate class

---
+++ src/CoreHook.ManagedHook/Remote/AssemblyDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreHook.ManagedHook.Remote
+{
+    internal class AssemblyDelegate
+    {
+        private readonly string _assemblyName;
+        private readonly string _typeName;
+        private readonly string _methodName;
+
+        internal AssemblyDelegate(string assemblyName, string typeName, string methodName)
+        {
+            _assemblyName = assemblyName;
+            _typeName = typeName;
+            _methodName = methodName;
+        }
+        public override string ToString()
+        {
+            return $"{_assemblyName}.{_typeName}.{_methodName}";
+        }
+    }
+}
