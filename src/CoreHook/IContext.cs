e479ea86dbe1da5b72a4b309f09c6a135c54a2e3
Thierry <unknownv2@users.noreply.github.com>
2018-09-29 00:23:29 -0400

    Add a Context interface which is always passed as the first parameter to every hooking library

---
+++ src/CoreHook/IContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreHook
+{
+    /// <summary>
+    /// The context passed to each CoreHook hooking C# library module
+    /// as the first parameter.
+    /// </summary>
+    public interface IContext
+    {
+    }
+}
