7d24b35f90a6ee103c9457d4567346c9f1d563fc
Thierry <unknownv2@users.noreply.github.com>
2018-09-30 14:53:23 -0400

    Remove unused BinaryLoaderArgs2 structure

--- src/CoreHook.BinaryInjection/BinaryLoader/BinaryLoaderArgs.cs
+++ src/CoreHook.BinaryInjection/BinaryLoader/BinaryLoaderArgs.cs
@@ -28,33 +28,4 @@ namespace CoreHook.BinaryInjection
             return encoding.GetBytes(path.PadRight(pathLength, '\0'));
         }
     }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct BinaryLoaderArgs2
-    {
-        [MarshalAs(UnmanagedType.U1)]
-        public bool Verbose;
-
-        [MarshalAs(UnmanagedType.U1)]
-        public bool WaitForDebugger;
-
-        [MarshalAs(UnmanagedType.U1)]
-        public bool StartAssembly;
-        [MarshalAs(UnmanagedType.U1)]
-        public bool Reserved1;
-        [MarshalAs(UnmanagedType.U1)]
-        public bool Reserved2;
-        [MarshalAs(UnmanagedType.U1)]
-        public bool Reserved3;
-        [MarshalAs(UnmanagedType.U1)]
-        public bool Reserved4;
-        [MarshalAs(UnmanagedType.U1)]
-        public bool Reserved5;
-
-        public byte[] PayloadFileName;
-
-        public byte[] CoreRootPath;
-
-        public byte[] CoreLibrariesPath;
-    }
 }
