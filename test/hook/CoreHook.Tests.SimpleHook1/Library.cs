55c7d8a63e92a78de734377da9cb9b26930caa8e
Thierry <unknownv2@users.noreply.github.com>
2018-10-04 19:31:26 -0400

    Convert SimpleHook1 test hooking library to use the IContext interface as the first parameter for the class

--- test/hook/CoreHook.Tests.SimpleHook1/Library.cs
+++ test/hook/CoreHook.Tests.SimpleHook1/Library.cs
@@ -4,12 +4,11 @@ namespace CoreHook.Tests.SimpleHook1
 {
     public class Library : IEntryPoint
     {
-        public Library(object context, string arg1)
+        public Library(IContext context, string arg1)
         {
-
         }
 
-        public void Run(object context, string message)
+        public void Run(IContext context, string message)
         {
             Console.WriteLine(message);
         }
