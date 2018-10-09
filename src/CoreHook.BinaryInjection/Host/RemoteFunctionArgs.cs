e78373875940380fc0518cbb31f2fb3f9d0a41d6
Thierry <unknownv2@users.noreply.github.com>
2018-10-08 20:05:38 -0400

    Make RemoteFunctionArgs a serializable class and implement the functions to allow formatting the native structure we pass to corerun as the CoreLoad initializing parameter

--- src/CoreHook.BinaryInjection/Host/RemoteFunctionArgs.cs
+++ src/CoreHook.BinaryInjection/Host/RemoteFunctionArgs.cs
@@ -1,14 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 using System.Text;
+using System.IO;
 
 namespace CoreHook.BinaryInjection
 {
-    [StructLayout(LayoutKind.Sequential)]
-    public struct RemoteFunctionArgs
+    public interface IBinarySerializer
     {
+        byte[] Serialize();
+        void Serialize(MemoryStream stream);
+    }
+    public class RemoteFunctionArgs : IBinarySerializer
+    {
+        public bool Is64BitProcess;
         public IntPtr UserData;
         public uint UserDataSize;
+        public byte[] Serialize()
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var writer = new BinaryWriter(ms))
+                {
+                    writer.Write(Is64BitProcess ? UserData.ToInt64() : UserData.ToInt32());
+                    writer.Write(UserDataSize);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        public void Serialize(MemoryStream stream)
+        {
+            using (var writer = new BinaryWriter(stream))
+            {
+                if(Is64BitProcess)
+                {
+                    writer.Write(UserData.ToInt64());
+                }
+                else
+                {
+                    writer.Write(UserData.ToInt32());
+                }
+                writer.Write(UserDataSize);
+            }
+        }
     }
 }
