6a68b77b2711d8bcb3f50031ef5acab98b757881
Thierry <unknownv2@users.noreply.github.com>
2018-10-12 04:03:24 -0400

    refactor: Remove unused function args constructors and make path encoding type a static variable

--- src/CoreHook.BinaryInjection/Host/FunctionCallArgs.cs
+++ src/CoreHook.BinaryInjection/Host/FunctionCallArgs.cs
@@ -31,155 +31,9 @@ namespace CoreHook.BinaryInjection
         private const int FunctionNameMaxWide = FunctionNameMax*2;
 
         private const int BinaryArgumentsSize = 12;
-        private const int FunctionNamePartsMin = 3;
 
-        public FunctionCallArgs(string classFunctionName, byte[] arguments = null)
-        {
-            if(classFunctionName == null)
-            {
-                throw new ArgumentNullException(classFunctionName);
-            }
-      
-            var args = classFunctionName.Split('.');
-            if (args.Length < FunctionNamePartsMin)
-            {
-                throw new ArgumentException("invalid class and function name");
-            }
+        private static Encoding PathEncoding = Encoding.Unicode;
 
-            string assembly = "";
-            var argsCount = args.Length - 2;
-            for (var x = 0; x < argsCount; x++)
-            {        
-                assembly += args[x];
-                if (x != argsCount - 1)
-                {
-                    assembly += ".";
-                }
-            }
-            var type = args[argsCount++];
-            var function = args[argsCount];
-
-            Assembly = Encoding.Unicode.GetBytes(assembly.PadRight(FunctionNameMax, '\0'));
-            Class = Encoding.Unicode.GetBytes(string.Format("{0}.{1}", assembly, type).PadRight(FunctionNameMax, '\0'));
-            Function = Encoding.Unicode.GetBytes(function.PadRight(FunctionNameMax, '\0'));
-            Arguments = arguments ?? new byte[BinaryArgumentsSize];
-        }
-
-        public FunctionCallArgs(string classFunctionName, IntPtr arguments)
-        {
-            if (classFunctionName == null)
-            {
-                throw new ArgumentNullException(classFunctionName);
-            }
-
-            var args = classFunctionName.Split('.');
-            if (args.Length < FunctionNamePartsMin)
-            {
-                throw new ArgumentException("invalid class and function name");
-            }
-            string assembly = "";
-            var argsCount = args.Length - 2;
-            for (var x = 0; x < argsCount; x++)
-            {
-                assembly += args[x];
-                if (x != argsCount - 1)
-                {
-                    assembly += ".";
-                }
-            }
-            var type = args[argsCount++];
-            var function = args[argsCount];
-
-            Assembly = Encoding.Unicode.GetBytes(assembly.PadRight(FunctionNameMax, '\0'));
-            Class = Encoding.Unicode.GetBytes(string.Format("{0}.{1}", assembly, type).PadRight(FunctionNameMax, '\0'));
-            Function = Encoding.Unicode.GetBytes(function.PadRight(FunctionNameMax, '\0'));
-            Arguments = arguments != null ? Binary.StructToByteArray(arguments, BinaryArgumentsSize) : new byte[BinaryArgumentsSize];
-        }
-
-        public FunctionCallArgs(string classFunctionName)
-        {
-            if (classFunctionName == null)
-            {
-                throw new ArgumentNullException(classFunctionName);
-            }
-
-            var args = classFunctionName.Split('.');
-            if (args.Length < FunctionNamePartsMin)
-            {
-                throw new ArgumentException("invalid class and function name");
-            }
-            string assembly = "";
-            var argsCount = args.Length - 2;
-            for (var x = 0; x < argsCount; x++)
-            {
-                assembly += args[x];
-                if (x != argsCount - 1)
-                {
-                    assembly += ".";
-                }
-            }
-            var type = args[argsCount++];
-            var function = args[argsCount];
-
-            Assembly = Encoding.Unicode.GetBytes(assembly.PadRight(FunctionNameMax, '\0'));
-            Class = Encoding.Unicode.GetBytes(string.Format("{0}.{1}", assembly, type).PadRight(FunctionNameMax, '\0'));
-            Function = Encoding.Unicode.GetBytes(function.PadRight(FunctionNameMax, '\0'));
-
-            Arguments = new byte[BinaryArgumentsSize];
-            //var argsStream = new MemoryStream(Arguments);
-            //var binStream = new BinaryWriter(argsStream);
-            //binStream.Write(arguments.UserData);
-            //binStream.Write(arguments.UserDataSize);
-            //binStream.Close();
-
-            //var formatter = new BinaryFormatter();
-            //formatter.Serialize(argsStream, arguments);
-
-            //var data = GCHandle.Alloc(argsStream.GetBuffer(), GCHandleType.Pinned);
-            //Arguments = argsStream.ToArray();// Binary.StructToByteArray(arguments, BinaryArgumentsSize);
-        }
-        public FunctionCallArgs(string classFunctionName, IBinarySerializer arguments)
-        {
-            if (classFunctionName == null)
-            {
-                throw new ArgumentNullException(classFunctionName);
-            }
-
-            var args = classFunctionName.Split('.');
-            if (args.Length < FunctionNamePartsMin)
-            {
-                throw new ArgumentException("invalid class and function name");
-            }
-            string assembly = "";
-            var argsCount = args.Length - 2;
-            for (var x = 0; x < argsCount; x++)
-            {
-                assembly += args[x];
-                if (x != argsCount - 1)
-                {
-                    assembly += ".";
-                }
-            }
-            var type = args[argsCount++];
-            var function = args[argsCount];
-
-            Assembly = Encoding.Unicode.GetBytes(assembly.PadRight(FunctionNameMax, '\0'));
-            Class = Encoding.Unicode.GetBytes(string.Format("{0}.{1}", assembly, type).PadRight(FunctionNameMax, '\0'));
-            Function = Encoding.Unicode.GetBytes(function.PadRight(FunctionNameMax, '\0'));
-
-            Arguments = arguments.Serialize();
-            //var argsStream = new MemoryStream(Arguments);
-            //var binStream = new BinaryWriter(argsStream);
-            //binStream.Write(arguments.UserData);
-            //binStream.Write(arguments.UserDataSize);
-            //binStream.Close();
-
-            //var formatter = new BinaryFormatter();
-            //formatter.Serialize(argsStream, arguments);
-
-            //var data = GCHandle.Alloc(argsStream.GetBuffer(), GCHandleType.Pinned);
-            //Arguments = argsStream.ToArray();// Binary.StructToByteArray(arguments, BinaryArgumentsSize);
-        }
         public FunctionCallArgs(IAssemblyDelegate assemblyDelegate, IBinarySerializer arguments)
         {
             if (assemblyDelegate == null)
@@ -187,34 +41,12 @@ namespace CoreHook.BinaryInjection
                 throw new ArgumentNullException("Assembly delegate was null");
             }   
 
-            Assembly = Encoding.Unicode.GetBytes(assemblyDelegate.AssemblyName.PadRight(FunctionNameMax, '\0'));
-            Class = Encoding.Unicode.GetBytes(string.Format("{0}.{1}", 
+            Assembly = PathEncoding.GetBytes(assemblyDelegate.AssemblyName.PadRight(FunctionNameMax, '\0'));
+            Class = PathEncoding.GetBytes(string.Format("{0}.{1}", 
                 assemblyDelegate.AssemblyName, assemblyDelegate.TypeName).PadRight(FunctionNameMax, '\0'));
-            Function = Encoding.Unicode.GetBytes(assemblyDelegate.MethodName.PadRight(FunctionNameMax, '\0'));
+            Function = PathEncoding.GetBytes(assemblyDelegate.MethodName.PadRight(FunctionNameMax, '\0'));
 
             Arguments = arguments.Serialize();
-            //arguments.Serialize(new MemoryStream(Arguments));
-        }
-  
-        public FunctionCallArgs(string assembly, string type, string function, byte[] arguments = null)
-        {
-            if (assembly == null)
-            {
-                throw new ArgumentNullException(assembly);
-            }
-            if (type == null)
-            {
-                throw new ArgumentNullException(type);
-            }
-            if (function == null)
-            {
-                throw new ArgumentNullException(function);
-            }
-
-            Assembly = Encoding.Unicode.GetBytes(assembly.PadRight(FunctionNameMax, '\0'));
-            Class = Encoding.Unicode.GetBytes(string.Format("{0}.{1}", assembly,type).PadRight(FunctionNameMax, '\0'));
-            Function = Encoding.Unicode.GetBytes(function.PadRight(FunctionNameMax, '\0'));
-            Arguments = arguments ?? new byte[BinaryArgumentsSize];
         }
     }
 }
