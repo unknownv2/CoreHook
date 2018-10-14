cba2aea18c9756579c869cf32583997bcdd78978
Thierry <unknownv2@users.noreply.github.com>
2018-10-13 20:10:49 -0400

    refactor: Use the null-coalescing operator instead of a negative integer for binary struct serializing

--- src/CoreHook.Unmanaged/Binary.cs
+++ src/CoreHook.Unmanaged/Binary.cs
@@ -7,15 +7,15 @@ namespace CoreHook.Unmanaged
 {
     public class Binary
     {
-        public static byte[] StructToByteArray(object obj, int length = -1)
+        public static byte[] StructToByteArray(object obj, int? length = null)
         {
-            var len = Marshal.SizeOf(obj);
-            var arr = new byte[length <= 0 ? len : length];
+            var objectLength = Marshal.SizeOf(obj);
+            var arr = new byte[length ?? objectLength];
 
-            var ptr = Marshal.AllocHGlobal(len);
+            var ptr = Marshal.AllocHGlobal(objectLength);
 
             Marshal.StructureToPtr(obj, ptr, false);
-            Marshal.Copy(ptr, arr, 0, len);
+            Marshal.Copy(ptr, arr, 0, objectLength);
             Marshal.FreeHGlobal(ptr);
 
             return arr;
