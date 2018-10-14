a51b8629d4561417af64186934b1f41477c56ffe
Thierry <unknownv2@users.noreply.github.com>
2018-10-13 22:22:23 -0400

    refactor: Remove unecessary using directives and commented out Dispose code methods

--- src/CoreHook.DependencyModel/Resolution/ResolverUtils.cs
+++ src/CoreHook.DependencyModel/Resolution/ResolverUtils.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿using System.IO;
 using Microsoft.Extensions.DependencyModel;
 
 namespace CoreHook.DependencyModel.Resolution
