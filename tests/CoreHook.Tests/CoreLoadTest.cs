89a65278e52e976a1619ce50c27ecbdef733d076
Thierry <unknownv2@users.noreply.github.com>
2018-10-20 18:17:00 -0400

    test: Add CoreLoadTest class for testing the CoreHook.CoreLoad project classes

---
+++ tests/CoreHook.Tests/CoreLoadTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CoreHook.Tests
+{
+    public class CoreLoadTest
+    {
+        [Fact]
+        public void ShouldThrowArgumentOutOfRange_For_InvalidRemoteParameter()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CoreLoad.Loader.Load(IntPtr.Zero));
+        }
+    }
+}
