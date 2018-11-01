e2ab04c4ac885f7b3b09e3eeab3d542282d65ea8
Thierry <unknownv2@users.noreply.github.com>
2018-11-01 00:08:04 -0400

    fix: Add MemoryAllocationType and MemoryProtectionType enums used by the MemoryHelper class

---
+++ src/CoreHook.Memory/MemoryProtectionType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreHook.Memory
+{
+    public enum MemoryProtectionType
+    {
+        Execute = 0x10,
+        ExecuteRead = 0x20,
+        ExecuteReadWrite = 0x40,
+        ExecuteWriteCopy = 0x80,
+        NoAccess = 0x01,
+        ReadOnly = 0x02,
+        ReadWrite = 0x04,
+        WriteCopy = 0x08,
+        Guard = 0x100,
+        NoCache = 0x200,
+        WriteCombine = 0x400
+    }
+}
