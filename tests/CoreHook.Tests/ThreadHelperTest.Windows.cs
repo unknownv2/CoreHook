7dc2ee7593ca400327012836cb7ac197b9477dde
Thierry <unknownv2@users.noreply.github.com>
2018-11-01 00:47:43 -0400

    test: Add ThreadHelper tests for testing function export address resolution

---
+++ tests/CoreHook.Tests/ThreadHelperTest.Windows.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Xunit;
+using CoreHook.Memory;
+
+namespace CoreHook.Tests
+{
+    public partial class ThreadHelperTest
+    {
+        [Fact]
+        public void ShouldGetFunctionAddressForCurrentProcess()
+        {
+            using (var processHandle = new ManagedProcess(Process.GetCurrentProcess()).SafeHandle)
+            {
+                string moduleFileName = Path.Combine(
+                        Environment.ExpandEnvironmentVariables("%Windir%"),
+                        "System32",
+                        "kernel32.dll");
+                const string functionName = "LoadLibraryW";
+
+                Assert.NotEqual(IntPtr.Zero,
+                    ThreadHelper.GetProcAddress(
+                        processHandle,
+                        moduleFileName,
+                        functionName));
+            }
+        }
+    }
+}
