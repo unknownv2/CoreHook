b13f8a9f1f8067bd597a55deb2aa9b56a471f1d1
Thierry <unknownv2@users.noreply.github.com>
2018-11-03 18:59:20 -0400

    refactor: Add IHookAccessControl to abstract thread ACL in the LocalHook class

---
+++ src/CoreHook/IHookAccessControl.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CoreHook
+{
+    /// <summary>
+    /// Manages the list of threads that are intercepted for each detour.
+    /// </summary>
+    public interface IHookAccessControl
+    {
+        bool IsExclusive { get; }
+        bool IsInclusive { get; }
+        void SetInclusiveACL(int[] acl);
+        void SetExclusiveACL(int[] acl);
+        int[] GetEntries();
+    }
+}
