2bcfae16dbf1a4deea7cf9259aeca26841c75ab9
Thierry <thierry44@gmail.com>
2018-11-21 12:04:54 -0500

    refactor: Move ProcessCreationConfig and CoreHookNativeConfig into their own class files
    * Since ProcessCreationConfig doesn't belong in the RemoteInjectorConfig class

---
+++ src/CoreHook.BinaryInjection/RemoteInjection/CoreHookNativeConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreHook.BinaryInjection.RemoteInjection
+{
+    public class CoreHookNativeConfig
+    {
+        /// <summary>
+        /// Library that initializes the .NET Core runtime (CoreCLR) and allows
+        /// loading and executing .NET Assemblies.
+        /// </summary>
+        public string HostLibrary { get; set; }
+        /// <summary>
+        /// Library that implements function intercept exports for the LocalHook class
+        /// such as DetourInstallHook.
+        /// </summary>
+        public string DetourLibrary { get; set; }
+        /// <summary>
+        /// Directory path which contains the main CoreCLR modules for hosting the runtime
+        /// such as CoreCLR and clrjit libraries.
+        /// </summary>
+        public string CoreCLRPath { get; set; }
+        /// <summary>
+        /// Directory path which contains the CoreCLR Assembly reference
+        /// runtime libraries such as System.Private.CoreLib.dll.
+        /// </summary>
+        public string CoreCLRLibrariesPath { get; set; }
+    }
+}
--- src/CoreHook.BinaryInjection/RemoteInjection/RemoteInjectorConfig.cs
+++ src/CoreHook.BinaryInjection/RemoteInjection/RemoteInjectorConfig.cs
@@ -1,44 +1,6 @@
 ﻿
 namespace CoreHook.BinaryInjection.RemoteInjection
 {
-    public class ProcessCreationConfig
-    {
-        /// <summary>
-        /// Filepath to an executable program on the system that is to be launched.
-        /// </summary>
-        public string ExecutablePath { get; set; }
-        /// <summary>
-        /// Arguments to be passed to the executable program when it is started
-        /// </summary>
-        public string CommandLine { get; set; }
-        /// <summary>
-        /// Attributes and options passed to the process creation function call.
-        /// </summary>
-        public uint ProcessCreationFlags { get; set; }
-    }
-    public class CoreHookNativeConfig
-    {
-        /// <summary>
-        /// Library that initializes the .NET Core runtime (CoreCLR) and allows
-        /// loading and executing .NET Assemblies.
-        /// </summary>
-        public string HostLibrary { get; set; }
-        /// <summary>
-        /// Library that implements function intercept exports for the LocalHook class
-        /// such as DetourInstallHook.
-        /// </summary>
-        public string DetourLibrary { get; set; }
-        /// <summary>
-        /// Directory path which contains the main CoreCLR modules for hosting the runtime
-        /// such as CoreCLR and clrjit libraries.
-        /// </summary>
-        public string CoreCLRPath { get; set; }
-        /// <summary>
-        /// Directory path which contains the CoreCLR Assembly reference
-        /// runtime libraries such as System.Private.CoreLib.dll.
-        /// </summary>
-        public string CoreCLRLibrariesPath { get; set; }
-    }
     public class RemoteInjectorConfig : CoreHookNativeConfig
     {
         /// <summary>
