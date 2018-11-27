8269975143d302ef763cff95a03078ad6e195fe9
Thierry <thierry44@gmail.com>
2018-11-27 07:25:11 -0500

    fix: Invert if statement and fix typo in ProcessExtensions.Is64Bit

--- src/CoreHook.Memory/ProcessExtensions.cs
+++ src/CoreHook.Memory/ProcessExtensions.cs
@@ -14,38 +14,38 @@ namespace CoreHook.Memory
             {
                 return Environment.Is64BitOperatingSystem;
             }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                if (!Environment.Is64BitOperatingSystem)
-                {
-                    return false;
-                }
+                throw new PlatformNotSupportedException("Platform not supported for detecting process architecture.");
+            }
+
+            if (!Environment.Is64BitOperatingSystem)
+            {
+                return false;
+            }
          
-                SafeProcessHandle processHandle = Interop.Kernel32.OpenProcess(
-                    Interop.Advapi32.ProcessOptions.PROCESS_QUERY_INFORMATION,
-                    false,
-                    process.Id);
+            SafeProcessHandle processHandle = Interop.Kernel32.OpenProcess(
+                Interop.Advapi32.ProcessOptions.PROCESS_QUERY_INFORMATION,
+                false,
+                process.Id);
 
-                if (processHandle.IsInvalid)
-                {
-                    throw new Win32Exception("Failed to open process query handle.");
-                }
-
-                using (processHandle)
-                {
-                    bool processIsWow64 = false;
-                    if (!Interop.Kernel32.IsWow64Process(processHandle, ref processIsWow64))
-                    {
-                        throw new Win32Exception("Determining process architecture with IsWow64Process failed.");
-                    }
-
-                    return !processIsWow64;
-                }
-            }
-            else
+            if (processHandle.IsInvalid)
             {
-                throw new PlatformNotSupportedException("Plaform not supported for detecting process architecture.");
+                throw new Win32Exception("Failed to open process query handle.");
             }
+
+            using (processHandle)
+            {
+                bool processIsWow64 = false;
+                if (!Interop.Kernel32.IsWow64Process(processHandle, ref processIsWow64))
+                {
+                    throw new Win32Exception("Determining process architecture with IsWow64Process failed.");
+                }
+
+                return !processIsWow64;
+            }
+
         }
     }
 }
