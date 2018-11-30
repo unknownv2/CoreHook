91a7ceca2dcdc7710df56369c20d51b81536fce0
Thierry <thierry44@gmail.com>
2018-11-29 20:00:09 -0500

    refactor: Clean up uwp named pipe creation code

--- examples/Uwp/CoreHook.Uwp.FileMonitor/Pipe/PipePlatform.cs
+++ examples/Uwp/CoreHook.Uwp.FileMonitor/Pipe/PipePlatform.cs
@@ -7,32 +7,30 @@ namespace CoreHook.Uwp.FileMonitor.Pipe
 {
     public class PipePlatform : IPipePlatform
     {
-        private static PipeSecurity CreateUWPPipeSecurity()
+        /// <summary>
+        /// Create the pipe security rules required for communicating with UWP applications.
+        /// </summary>
+        /// <returns>The pipe access control for communicating with UWP applications.</returns>
+        private static PipeSecurity CreateUwpPipeSecurity()
         {
             const PipeAccessRights pipeAccess = PipeAccessRights.ReadWrite;
             const AccessControlType accessControl = AccessControlType.Allow;
+
             var pipeSecurity = new PipeSecurity();
 
             using (var identity = WindowsIdentity.GetCurrent())
             {
-                pipeSecurity.AddAccessRule(
-                      new PipeAccessRule(identity.User, pipeAccess, accessControl)
-                );
+                pipeSecurity.AddAccessRule(new PipeAccessRule(identity.User, pipeAccess, accessControl));
                 if (identity.User != identity.Owner)
                 {
-                    pipeSecurity.AddAccessRule(
-                        new PipeAccessRule(identity.Owner, pipeAccess, accessControl)
-                    );
+                    pipeSecurity.AddAccessRule(new PipeAccessRule(identity.Owner, pipeAccess, accessControl));
                 }
-                pipeSecurity.AddAccessRule(new PipeAccessRule(
-                    identity.User, pipeAccess, accessControl));
- 
+                pipeSecurity.AddAccessRule(new PipeAccessRule(identity.User, pipeAccess, accessControl));
             }
 
             pipeSecurity.AddAccessRule(
                 new PipeAccessRule(
-                    new SecurityIdentifier(WellKnownSidType.AuthenticatedUserSid, null), pipeAccess, accessControl)
-            );
+                    new SecurityIdentifier(WellKnownSidType.AuthenticatedUserSid, null), pipeAccess, accessControl));
 
             pipeSecurity.AddAccessRule(new PipeAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), pipeAccess, accessControl));
             pipeSecurity.AddAccessRule(new PipeAccessRule(new SecurityIdentifier("S-1-15-2-1"), pipeAccess, accessControl));
@@ -40,9 +38,14 @@ namespace CoreHook.Uwp.FileMonitor.Pipe
             return pipeSecurity;
         }
 
+        /// <summary>
+        /// Create a named pipe server that allows communicating with UWP applications.
+        /// </summary>
+        /// <param name="pipeName">The name of the server pipe to create.</param>
+        /// <returns>The named pipe used for communicating with UWP applications.</returns>
         public NamedPipeServerStream CreatePipeByName(string pipeName)
         {
-            return NamedPipeNative.CreateNamedServerPipe(".", "pipe", pipeName, CreateUWPPipeSecurity());
+            return NamedPipeNative.CreateNamedServerPipe(".", "pipe", pipeName, CreateUwpPipeSecurity());
         }
     }
 }
