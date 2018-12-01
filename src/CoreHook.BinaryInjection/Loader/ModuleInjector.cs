c34b7c2a69db0566c03dc355d6c7151d22f2f3c6
Thierry <thierry44@gmail.com>
2018-12-01 11:49:46 -0500

    refactor: Remove unused BinaryLoader class
    We are using the AssemblyLoader class so we don't need that one anymore.

--- src/CoreHook.BinaryInjection/Loader/ModuleInjector.cs
+++ src/CoreHook.BinaryInjection/Loader/ModuleInjector.cs
@@ -1,19 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using CoreHook.Memory;
+﻿using CoreHook.Memory;
 
 namespace CoreHook.BinaryInjection.Loader
 {
     internal class ModuleInjector : IModuleInjector
     {
-        private IProcessManager _processManager;
+        private readonly IProcessManager _processManager;
 
         internal ModuleInjector(IProcessManager processManager)
         {
             _processManager = processManager;
         }
 
+        /// <summary>
+        /// Load a module into managed process.
+        /// </summary>
+        /// <param name="path">File path of the module to be loaded.</param>
         public void Inject(string path) => _processManager.InjectBinary(path);
     }
 }
