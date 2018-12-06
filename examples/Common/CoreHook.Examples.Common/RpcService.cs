670a329fd963cfbc0dfca920af377fc95221876c
Thierry <thierry44@gmail.com>
2018-12-06 00:47:43 -0500

    refactor: Remove abbreviations from the UWP example variables and methods

--- examples/Common/CoreHook.Examples.Common/RpcService.cs
+++ examples/Common/CoreHook.Examples.Common/RpcService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using CoreHook.IPC;
 using CoreHook.IPC.NamedPipes;
 using CoreHook.IPC.Platform;
 using CoreHook.IPC.Transport;
