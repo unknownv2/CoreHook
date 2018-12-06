457f812987c1c144a35434de76ef9718b83334d2
Thierry <thierry44@gmail.com>
2018-12-06 00:09:39 -0500

    refactor: Add PipeConnection class to IPC project

---
+++ src/CoreHook.IPC/PipeConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace CoreHook.IPC
+{
+    internal class PipeConnection : IConnection
+    {
+        public Stream Stream => _stream;
+
+        private readonly Func<bool> _connectionEnded;
+        private readonly PipeStream _stream;
+
+        public PipeConnection(PipeStream stream, Func<bool> connectionEnded)
+        {
+            _connectionEnded = connectionEnded;
+            _stream = stream;
+        }
+
+        public bool IsConnected => !_connectionEnded() && _stream.IsConnected;
+    }
+}
