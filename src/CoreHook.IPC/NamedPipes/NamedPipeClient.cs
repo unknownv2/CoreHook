c141b90264d1bd9ec7aeac053229a52e6ee23528
Thierry <thierry44@gmail.com>
2018-12-06 00:22:45 -0500

    refactor: Simplify the named pipe client implementation
    * Implement a message handler to send and receive message

--- src/CoreHook.IPC/NamedPipes/NamedPipeClient.cs
+++ src/CoreHook.IPC/NamedPipes/NamedPipeClient.cs
@@ -2,37 +2,37 @@
 using System.IO;
 using System.IO.Pipes;
 using System.Security.Principal;
-using CoreHook.IPC.Messages;
+using CoreHook.IPC.Handlers;
+using CoreHook.IPC.Transport;
 
 namespace CoreHook.IPC.NamedPipes
 {
     public class NamedPipeClient : INamedPipeClient
     {
+        public IConnection Connection { get; private set; }
+        public IMessageHandler MessageHandler { get; private set; }
+
+        private NamedPipeClientStream _pipeStream;
+
+        private bool _connectionStopped;
+
         private readonly string _pipeName;
-       
-        private NamedPipeClientStream _clientStream;
-
-        private StreamReader _reader;
-        private StreamWriter _writer;
-
-        public PipeStream PipeStream => _clientStream;
-
-        private const string ServerName = ".";
 
         public NamedPipeClient(string pipeName)
         {
             _pipeName = pipeName;
+            _connectionStopped = false;
         }
 
         public static PipeStream CreatePipeStream(string pipeName)
         {
             var client = new NamedPipeClient(pipeName);
-            return client.Connect() ? client.PipeStream : null;
+            return client.Connect() ? client._pipeStream : null;
         }
 
         public bool Connect(int timeoutMilliseconds = 10000)
         {
-            if (_clientStream != null)
+            if (_pipeStream != null)
             {
                 throw new InvalidPipeOperationException("Client pipe already connected");
             }
@@ -40,98 +40,39 @@ namespace CoreHook.IPC.NamedPipes
             {
                 throw new InvalidPipeOperationException("Client pipe name was not set");
             }
+
             try
             {
-                _clientStream = new NamedPipeClientStream(
-                    ServerName,
+                _pipeStream = new NamedPipeClientStream(
+                    ".",
                     _pipeName,
                     PipeDirection.InOut,
                     PipeOptions.Asynchronous,
                     TokenImpersonationLevel.Impersonation);
 
-                _clientStream.Connect(timeoutMilliseconds);
+                _pipeStream.Connect(timeoutMilliseconds);
             }
             catch (TimeoutException)
             {
                 return false;
             }
-            catch (IOException)
+            catch (IOException ex)
             {
+                Console.WriteLine(ex.ToString());
                 return false;
             }
 
-            _reader = new StreamReader(_clientStream);
-            _writer = new StreamWriter(_clientStream);
+            Connection = new PipeConnection(_pipeStream, () => _connectionStopped);
+            MessageHandler = new MessageHandler(Connection);
 
             return true;
         }
 
-        public bool TrySendRequest(Message message)
-        {
-            try
-            {
-                SendRequest(message);
-                return true;
-            }
-            catch (InvalidPipeOperationException)
-            {
-            }
-            return false;
-        }
-
-        public void SendRequest(Message message)
-        {
-            SendRequest(message.ToString());
-        }
-
-        public void SendRequest(string message)
-        {
-            ValidateConnection();
-
-            try
-            {
-                _writer.WriteLine(message);
-                _writer.Flush();
-            }
-            catch (IOException e)
-            {
-                throw new InvalidPipeOperationException("Unable to send: " + message, e);
-            }
-        }
-
-        public string ReadRawResponse()
-        {
-            try
-            {
-                string response = _reader.ReadLine();
-                if (response == null)
-                {
-                    throw new InvalidPipeOperationException("Unable to read from pipe", null);
-                }
-                return response;
-            }
-            catch (IOException e)
-            {
-                throw new InvalidPipeOperationException("Unable to read from pipe", e);
-            }
-        }
-        private void ValidateConnection()
-        {
-            if (_clientStream == null)
-            {
-                throw new InvalidPipeOperationException("There is no connection");
-            }
-        }
-
         public void Dispose()
         {
-            ValidateConnection();
-
-            _clientStream?.Dispose();
-            _clientStream = null;
-
-            _reader = null;
-            _writer = null;
+            _connectionStopped = true;
+            _pipeStream?.Dispose();
+            _pipeStream = null;
         }
     }
 }
