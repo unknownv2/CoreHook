c7596c9553311456ceee6a10c0769441ce2515b6
Thierry <thierry44@gmail.com>
2018-12-08 15:01:54 -0500

    doc: Add more notes and details to the HookFactory code

--- src/CoreHook/HookFactory.cs
+++ src/CoreHook/HookFactory.cs
@@ -10,10 +10,10 @@ namespace CoreHook
         /// <summary>
         /// Create a managed hook.
         /// </summary>
-        /// <param name="targetFunction"></param>
-        /// <param name="detourFunction"></param>
-        /// <param name="callback"></param>
-        /// <returns></returns>
+        /// <param name="targetFunction">The target function address that will be detoured.</param>
+        /// <param name="detourFunction">The detour function that will be called instead of the function at <paramref name="targetFunction"/>.</param>
+        /// <param name="callback">A context object that can be accessed as <see cref="HookRuntimeInfo.Callback"/>.</param>
+        /// <returns>The handle to the function hook.</returns>
         public static IHook CreateHook(IntPtr targetFunction, Delegate detourFunction, object callback)
         {
             return LocalHook.Create(targetFunction, detourFunction, callback);
@@ -22,9 +22,9 @@ namespace CoreHook
         /// <summary>
         ///  Create an unmanaged hook.
         /// </summary>
-        /// <param name="targetFunction"></param>
-        /// <param name="detourFunction"></param>
-        /// <returns></returns>
+        /// <param name="targetFunction">The target function address that will be detoured.</param>
+        /// <param name="detourFunction">The detour function that will be called instead of the function at <paramref name="targetFunction"/>.</param>
+        /// <returns>The handle to the function hook.</returns>
         public static IHook CreateHook(IntPtr targetFunction, IntPtr detourFunction)
         {
             return LocalHook.CreateUnmanaged(targetFunction, detourFunction, IntPtr.Zero);
@@ -33,10 +33,10 @@ namespace CoreHook
         /// <summary>
         /// Create an unmanaged hook
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="targetFunction"></param>
-        /// <param name="detourFunction"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">The delegate type representing the detoured function signature.</typeparam>
+        /// <param name="targetFunction">The target function address that will be detoured.</param>
+        /// <param name="detourFunction">The detour function that will be called instead of the function at <paramref name="targetFunction"/>.</param>
+        /// <returns>The handle to the function hook.</returns>
         public static IHook<T> CreateHook<T>(IntPtr targetFunction, IntPtr detourFunction) where T : class
         {
             return LocalHook<T>.CreateUnmanaged(targetFunction, detourFunction, IntPtr.Zero);
@@ -45,11 +45,11 @@ namespace CoreHook
         /// <summary>
         /// Create a managed hook.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="targetFunction"></param>
-        /// <param name="detourFunction"></param>
-        /// <param name="callback"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">The delegate type representing the detoured function signature.</typeparam>
+        /// <param name="targetFunction">The target function address that will be detoured.</param>
+        /// <param name="detourFunction">The detour function that will be called instead of the function at <paramref name="targetFunction"/>.</param>
+        /// <param name="callback">A context object that can be accessed as <see cref="HookRuntimeInfo.Callback"/>.</param>
+        /// <returns>The handle to the function hook.</returns>
         public static IHook<T> CreateHook<T>(IntPtr targetFunction, T detourFunction, object callback = null) where T : class
         {
             return LocalHook<T>.Create(targetFunction, detourFunction as Delegate, callback);
