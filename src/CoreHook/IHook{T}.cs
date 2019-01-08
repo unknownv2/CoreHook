4997777ee2c213e7c86c9eb540d83dd43cfd6e74
Thierry <thierry44@gmail.com>
2019-01-08 00:02:44 -0500

    refactor: Move typed class and interface into separate files

---
+++ src/CoreHook/IHook{T}.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CoreHook
+{
+    /// <summary>
+    /// Interface for a hooking class that manages installs and uninstalls of function detours.
+    /// It requires a defining a function delegate type for the function that is being detoured.
+    /// </summary>
+    /// <typeparam name="T">A type representing the target function's delegate or signature.</typeparam>
+    public interface IHook<out T> : IHook where T : class
+    {
+        /// <summary>
+        /// The original function address, called to bypass the detour.
+        /// </summary>
+        T Original { get; }
+        /// <summary>
+        /// The target address where the detour is installed to.
+        /// </summary>
+        T Target { get; }
+    }
+}
