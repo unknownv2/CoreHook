475e2387671cd6315e4cc01c918eab84c19bc994
Thierry <thierry44@gmail.com>
2019-03-17 14:48:58 -0400

    doc: Add comment details to some IPC classes

--- src/CoreHook.IPC/Messages/IMessage.cs
+++ src/CoreHook.IPC/Messages/IMessage.cs
@@ -1,6 +1,9 @@
 ﻿
 namespace CoreHook.IPC.Messages
 {
+    /// <summary>
+    /// Interface for defining a message that can be sent and received between processes.
+    /// </summary>
     public interface IMessage
     {
         string Header { get; }
--- src/CoreHook.IPC/NamedPipes/INamedPipe.cs
+++ src/CoreHook.IPC/NamedPipes/INamedPipe.cs
@@ -3,8 +3,15 @@ using CoreHook.IPC.Transport;
 
 namespace CoreHook.IPC.NamedPipes
 {
+    /// <summary>
+    /// Interface defining a named pipe communication channel.
+    /// </summary>
     public interface INamedPipe : ITransportChannel, IDisposable
     {
+        /// <summary>
+        /// Initialize the pipe connection.
+        /// </summary>
+        /// <returns>True if initialization completed successfully.</returns>
         bool Connect();
     }
 }
