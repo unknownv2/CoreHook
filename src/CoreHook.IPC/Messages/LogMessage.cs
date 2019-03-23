1972b7c932e6da233528331bcf58821075a893c2
Thierry <thierry44@gmail.com>
2019-03-23 19:12:38 -0400

    refactor: Remove unused notification class
    * Add more information about the log message classes

--- src/CoreHook.IPC/Messages/LogMessage.cs
+++ src/CoreHook.IPC/Messages/LogMessage.cs
@@ -1,58 +1,63 @@
 ﻿using System;
 namespace CoreHook.IPC.Messages
 {
+    /// <summary>
+    /// Defines the types of message information.
+    /// </summary>
     public enum LogLevel
     {
+        /// <summary>
+        /// Debug diagnostic message.
+        /// </summary>
         Debug = 0,
+        /// <summary>
+        /// Release diagnostic message.
+        /// </summary>
         Trace,
+        /// <summary>
+        /// General information message.
+        /// </summary>
         Info,
+        /// <summary>
+        /// Warning message.
+        /// </summary>
         Warning,
+        /// <summary>
+        /// Error message.
+        /// </summary>
         Error
     }
 
-    public class LogMessageNotification
-    {
-        public const string Message = "LogMessage";
-        public LogMessage RequestData { get; }
-
-        public LogMessageNotification(string body)
-        {
-            RequestData = LogMessage.FromBody(body);
-        }
-
-        public LogMessageNotification(LogLevel level, string message)
-        {
-            RequestData = new LogMessage(level, message);
-        }
-
-        public IMessage CreateMessage()
-        {
-            return new Message(Message, RequestData.ToMessage());
-        }
-
-        public static IMessage CreateMessage(LogLevel level, string message)
-        {
-            return new LogMessageNotification(level, message).CreateMessage();
-        }
-
-        public static LogMessage ParseMessage(IMessage message)
-        {
-            return LogMessage.FromBody(message.Body);
-        }
-    }
-
+    /// <summary>
+    /// A message containing application status information.
+    /// </summary>
     public class LogMessage : CustomMessage
     {
+        /// <summary>
+        /// The message type.
+        /// </summary>
         public LogLevel Level { get; }
-
+        /// <summary>
+        /// The message data.
+        /// </summary>
         public string Message { get; }
 
+        /// <summary>
+        /// Initialize a new instance of the <see cref="LogMessage"/> class.
+        /// </summary>
+        /// <param name="level">The message type.</param>
+        /// <param name="message">The message data.</param>
         public LogMessage(LogLevel level, string message)
         {
             Level = level;
             Message = message;
         }
 
+        /// <summary>
+        /// Parse a log message from it's string data.
+        /// </summary>
+        /// <param name="body">The message data.</param>
+        /// <returns>A new instance of <see cref="LogMessage"/>.</returns>
         internal static LogMessage FromBody(string body)
         {
             if (string.IsNullOrEmpty(body))
@@ -75,6 +80,7 @@ namespace CoreHook.IPC.Messages
             return new LogMessage(logLevel, dataParts[1]);
         }
 
+        /// <inheritdoc />
         public override string ToMessage()
         {
             return string.Join(MessageSeparator.ToString(), Level, Message);
